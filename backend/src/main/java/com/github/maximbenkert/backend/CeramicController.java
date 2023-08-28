24d937c0e2a9684a36154f8329be385251d471dc
MaximBenkert <max.benkert@posteo.de>
2023-08-28 12:26:28 +0200

    connect backend and frontend (axios, react-router-dom), add CeramicController

---
+++ backend/src/main/java/com/github/maximbenkert/backend/CeramicController.java
@@ -0,0 +1,15 @@
+package com.github.maximbenkert.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ceramics")
+public class CeramicController {
+
+    @GetMapping
+    String ceramic(){
+        return "Ton ist Ton";
+    }
+}
