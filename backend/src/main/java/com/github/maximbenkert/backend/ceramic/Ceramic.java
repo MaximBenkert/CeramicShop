9f51c6a94aed4c83a9da63bdef638ede5a752d2b
MaximBenkert <max.benkert@posteo.de>
2023-08-29 18:02:54 +0200

    connect Ceramic with local MongoDB

--- backend/src/main/java/com/github/maximbenkert/backend/ceramic/Ceramic.java
+++ backend/src/main/java/com/github/maximbenkert/backend/ceramic/Ceramic.java
@@ -1,7 +1,9 @@
 package com.github.maximbenkert.backend.ceramic;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document("ceramics")
 public record Ceramic(
         @Id
         String id,
