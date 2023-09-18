0a2b844aa424acba8fb8510cecf78ce72d2fde14
MaximBenkert <max.benkert@posteo.de>
2023-09-18 16:17:20 +0200

    IntegrationTest getAllCeramics when Repo is Empty

--- backend/src/test/java/com/github/maximbenkert/backend/ceramic/CeramicIntegrationTest.java
+++ backend/src/test/java/com/github/maximbenkert/backend/ceramic/CeramicIntegrationTest.java
@@ -5,11 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @SpringBootTest
