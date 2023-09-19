267135be09e06d3ded2570ab3af87bbe0f05b375
MaximBenkert <max.benkert@posteo.de>
2023-09-19 23:24:30 +0200

    frontend: delete unused component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import axios from "axios";
-import {Ceramic, NewCeramic} from "./models/Ceramic";
+import {Ceramic} from "./models/Ceramic";
 import CeramicGallery from "./components/CeramicGallery";
 
 function App() {
 
   const [ceramics, setCeramics] = useState<Ceramic[]> ([]);
-  const [ceramic, setCeramic] = useState<Ceramic>({id: "", name: "", description: "", price: 0.0})
 
   useEffect(() =>{
     loadAllCeramics()
