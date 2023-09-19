5a6697094b8f877240bae903416149828858106b
MaximBenkert <max.benkert@posteo.de>
2023-09-19 20:56:27 +0200

    frontend: function loadAllCeramics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import axios from "axios";
+import {Ceramic, NewCeramic} from "./models/Ceramic";
+import CeramicGallery from "./components/CeramicGallery";
 
 function App() {
 
-  const [message, setMessage] = useState<string> ("");
+  const [ceramics, setCeramics] = useState<Ceramic[]> ([]);
+  const [ceramic, setCeramic] = useState<Ceramic>({id: "", name: "", description: "", price: 0.0})
 
-  useEffect(() => {
-    axios.get("/api/ceramics")
-        .then(response => response.data)
-        .then(setMessage)
+  useEffect(() =>{
+    loadAllCeramics()
   }, [])
+
+  function loadAllCeramics() {
+    axios.get("/api/ceramics")
+        .then((getAllCeramicsResponse) =>
+        {
+          setCeramics(getAllCeramicsResponse.data)
+        })
+        .catch(reason => console.error(reason))
+  }
 
 
   return (
-    <div>
-      {message}
-    </div>
+
+      <CeramicGallery ceramics={ceramics}/>
+
   );
 }
 
