0f02d92ed828a26f7740ae3cdae22da59fd04956
MaximBenkert <max.benkert@posteo.de>
2023-09-19 23:12:52 +0200

    frontend: styled components

--- frontend/src/components/CeramicGallery.tsx
+++ frontend/src/components/CeramicGallery.tsx
@@ -1,20 +1,26 @@
-import {Ceramic} from "../models/Ceramic";
-import CeramicCard from "./CeramicCard";
 import React from 'react';
+import styled from 'styled-components';
+import { Ceramic } from '../models/Ceramic';
+import CeramicCard from './CeramicCard';
 
-type Props = {
-    ceramics: Ceramic []
+// Stilisierte Komponente für die Galerie erstellen
+const GalleryContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap; // Elemente werden umgebrochen, wenn der Platz nicht ausreicht
+    justify-content: space-evenly; // Abstand zwischen den Karten
+`;
+
+export type Props = {
+    ceramics: Ceramic[];
 }
 
 export default function CeramicGallery(props: Props) {
-    return(
-        <div>
-            {props.ceramics.map((ceramic)=>{
-                return (
-                    <CeramicCard key={ceramic.id} ceramic={ceramic}/>
-                )
-            })
-            }
-        </div>
+    return (
+        <GalleryContainer>
+            {props.ceramics.map((ceramic) => (
+                <CeramicCard key={ceramic.id} ceramic={ceramic} />
+            ))}
+        </GalleryContainer>
     )
 }
