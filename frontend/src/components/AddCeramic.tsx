de31e69f473c73cca9535bcecc81788a5502021a
MaximBenkert <max.benkert@posteo.de>
2023-10-05 11:53:07 +0200

    addCeramic integration Test

--- frontend/src/components/AddCeramic.tsx
+++ frontend/src/components/AddCeramic.tsx
@@ -1,17 +1,24 @@
-import {Ceramic} from "../models/Ceramic";
-import {FormEvent, useState} from "react";
+import { Ceramic } from "../models/Ceramic";
+import { FormEvent, useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
   background-color: #9cbac6;
   display: flex;
   flex-direction: column;
-  justify-content: space-evenly;
-  flex: 1;
+  justify-content: center;
+  align-items: center;
+  height: 30vh;
 `;
 
 const Form = styled.form`
   color: #6699cc;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  padding: 20px;
+  border-radius: 8px;
 `;
 
 const MyButton = styled.button`
@@ -22,26 +29,43 @@ const MyButton = styled.button`
   cursor: pointer;
 `;
 
-type Props = {
-    ceramics: Ceramic []
-    addCeramic: (newCeramic: Ceramic) => void
-}
+const InputGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+`;
 
-export default function AddCeramic (props: Props)  {
-    const [newCeramic, setNewCeramic] = useState<Ceramic>({id: "", name: "hi", description: "", price: 0.00})
+const Label = styled.label`
+  margin-bottom: 5px;
+`;
+
+type Props = {
+    ceramics: Ceramic[];
+    addCeramic: (newCeramic: Ceramic) => void;
+};
+
+export default function AddCeramic(props: Props) {
+    const [newCeramic, setNewCeramic] = useState<Ceramic>({
+        id: "",
+        name: "hi",
+        description: "",
+        price: 0.0,
+    });
+
     function onSaveCeramic(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault()
+        event.preventDefault();
         if (newCeramic.name === undefined || newCeramic.name === "") {
-            console.error("name required")
-            return
+            console.error("name required");
+            return;
         }
-        props.addCeramic(newCeramic)
+        props.addCeramic(newCeramic);
     }
 
-        return (
-            <Container>
-                <Form onSubmit={onSaveCeramic}>
-                    <label>Name</label>
+    return (
+        <Container>
+            <Form onSubmit={onSaveCeramic}>
+                <InputGroup>
+                    <Label>Name</Label>
                     <input
                         type="text"
                         required
@@ -50,25 +74,36 @@ export default function AddCeramic (props: Props)  {
                             setNewCeramic({ ...newCeramic, name: event.target.value })
                         }
                     />
-                    <label>Beschreibung</label>
+                </InputGroup>
+                <InputGroup>
+                    <Label>Beschreibung</Label>
                     <input
                         type="text"
+                        required
                         value={newCeramic.description}
                         onChange={(event) =>
-                            setNewCeramic({ ...newCeramic, description: event.target.value })
+                            setNewCeramic({
+                                ...newCeramic,
+                                description: event.target.value,
+                            })
                         }
                     />
-                    <label>Preis</label>
+                </InputGroup>
+                <InputGroup>
+                    <Label>Preis</Label>
                     <input
                         type="number"
                         value={newCeramic.price}
                         onChange={(event) =>
-                            setNewCeramic({ ...newCeramic, price: parseFloat(event.target.value) })
+                            setNewCeramic({
+                                ...newCeramic,
+                                price: parseFloat(event.target.value),
+                            })
                         }
                     />
-                    <MyButton type="submit">Save</MyButton>
-                </Form>
-            </Container>
-        )
+                </InputGroup>
+                <MyButton type="submit">Save</MyButton>
+            </Form>
+        </Container>
+    );
 }
-
