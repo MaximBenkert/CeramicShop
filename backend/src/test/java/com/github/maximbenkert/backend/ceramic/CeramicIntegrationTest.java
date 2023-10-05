0df659c13b5a97e0c4c6280e4391c9250c3ca6e8
MaximBenkert <max.benkert@posteo.de>
2023-10-05 11:14:19 +0200

    addCeramic integration Test

--- backend/src/test/java/com/github/maximbenkert/backend/ceramic/CeramicIntegrationTest.java
+++ backend/src/test/java/com/github/maximbenkert/backend/ceramic/CeramicIntegrationTest.java
@@ -1,20 +1,32 @@
 package com.github.maximbenkert.backend.ceramic;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import java.math.BigDecimal;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+
 class CeramicIntegrationTest {
     @Autowired
     MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    Ceramic ceramicWithoutId = new Ceramic(null, "Vase", "Schöne Vase", new BigDecimal("29.99"));
+
+
 
     @Test
     void getAllCeramics_shouldReturnEmptyList_whenRepoIsEmpty() throws Exception {
@@ -23,6 +35,18 @@ class CeramicIntegrationTest {
                 .andExpect(content().json("""
                         []
                         """));
+    }
+
+    @Test
+    void addCeramic_shouldAddCeramicToDatabase () throws Exception {
+        mockMvc.perform(post("/api/ceramics")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(ceramicWithoutId)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists()) // Überprüfen Sie, ob die Antwort ein ID-Feld enthält
+                .andExpect(jsonPath("$.name").value("Vase")) // Überprüfen Sie, ob das Name-Feld korrekt ist
+                .andExpect(jsonPath("$.description").value("Schöne Vase")) // Überprüfen Sie, ob das Description-Feld korrekt ist
+                .andExpect(jsonPath("$.price").value(29.99)); // Ü
 
     }
 }
