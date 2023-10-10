509821f01cd391feb5dc1c77c1b8b3a795aad457
MaximBenkert <max.benkert@posteo.de>
2023-10-10 13:28:22 +0200

    styling frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,41 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
 import CeramicGallery from "./components/CeramicGallery";
 import useCeramics from "./hooks/useCeramics";
 import AddCeramic from "./components/AddCeramic";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CeramicDetails from "./components/CeramicDetails";
 
+// Styled Components
+const AppContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const ContentContainer = styled.div`
+    width: 100%;
+    max-width: 1000px;
+`;
+
 function App() {
     const { ceramic, loadCeramicById, ceramics, addCeramics } = useCeramics();
 
     return (
         <BrowserRouter>
-            <div>
+            <AppContainer>
                 <Routes>
-                    <Route path="/" element={<div>
+                    <Route path="/" element={<ContentContainer>
                         <AddCeramic addCeramic={addCeramics} />
                         <CeramicGallery ceramics={ceramics} />
-                    </div>} />
+                    </ContentContainer>} />
 
                     <Route path="/details/:id"
                            element={
                                <CeramicDetails ceramic={ceramic} loadCeramicById={loadCeramicById}></CeramicDetails>
-                        } />
+                           } />
                 </Routes>
-            </div>
+            </AppContainer>
         </BrowserRouter>
     );
 }
